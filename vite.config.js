78efaebf31cce08aa5df86483ef204d760dc4e89
Kurosobi <ivansidelnikov259@gmail.com>
2025-12-04 15:49:27 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/color-palette-generator/',
+  base: '/VUE_PROJECT/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
